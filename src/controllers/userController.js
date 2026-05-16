b97699b4b04cf2aa4d1ee3e423b84332e27af80c
Lucas Queiroz <lqvieira7@gmail.com>
2026-05-16 20:07:22 -0300

    feat: POST e GET users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,12 @@
-export const getHome = (req, res) => {
-  res.send('helo World');
+import { createUser, getAllUsers } from '../services/userService.js';
+
+export const postUser = async (req, res) => {
+  const { name, email } = req.body;
+  const result = await createUser({ name, email });
+  res.status(201).json(result);
 };
-export const getUsers = (req, res) => {
-  res.send('usuarios');
+
+export const getUsers = async (req, res) => {
+  const users = await getAllUsers();
+  res.status(200).json(users);
 };
