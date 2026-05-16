30f9fa7ea33b8a6990942bf074cb202749a2108a
Lucas Queiroz <lqvieira7@gmail.com>
2026-05-16 20:08:26 -0300

    feat: SCHEMA User

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
